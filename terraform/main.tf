b07ff896e4d48c4bda4aa661879bdb82eb3ce2fc
Rakshitha Umesh <159748251+RakshithaUmesh1@users.noreply.github.com>
2025-08-02 18:45:34 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,25 +1,24 @@
 provider "aws" {
-    region = "ap-southeast-1"
+    region = "us-east-1"
 }
 
 resource "aws_instance" "instance-1" {
-  ami = "ami-02c7683e4ca3ebf58 "
+  ami           = "ami-0fff1b9a61dec8a5f"
   instance_type = "t2.micro"
   security_groups = ["default"]
-  key_name = "my-key"
+  key_name = "jun26"
   tags ={
     Name = "grafana-server"
   }
 }
 
 resource "aws_instance" "instance-2" {
-  ami = "ami-02c7683e4ca3ebf58 "
+  ami           = "ami-0fff1b9a61dec8a5f"
   instance_type = "t2.micro"
   security_groups = ["default"]
-  key_name = "my-key"
+  key_name = "jun26"
   tags ={
     Name = "node-expo"
   }
 }
-
 
