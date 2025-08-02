bc4c1e69fa3b4ffe753738f767305f81806d4ea3
Rakshitha Umesh <159748251+RakshithaUmesh1@users.noreply.github.com>
2025-08-02 16:31:39 +0530

    Update main.tf

--- eks/main.tf
+++ eks/main.tf
@@ -99,7 +99,7 @@ resource "aws_eks_cluster" "microdegree" {
 resource "aws_eks_node_group" "microdegree" {
   cluster_name    = aws_eks_cluster.microdegree.name
   node_group_name = "microdegree-node-group"
-  node_role_arn   = aws_iam_role.microdegree_node_group_role.arn
+  node_role_arn   = aws_iam_role.microdegree_node_group_role1.arn
   subnet_ids      = aws_subnet.microdegree_subnet[*].id
 
   scaling_config {
@@ -136,7 +136,7 @@ EOF
 }
 
 resource "aws_iam_role_policy_attachment" "microdegree_cluster_role_policy" {
-  role       = aws_iam_role.microdegree_cluster_role.name
+  role       = aws_iam_role.microdegree_cluster_role1.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
